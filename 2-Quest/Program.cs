91695354384c0ef0aa42f55bf7c55830ccace500
Kaio Maia <98839071+KaioMR@users.noreply.github.com>
2022-02-02 13:24:08 -0300

    Add files via upload

---
+++ 2-Quest/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int total = 3;
+        Person[] p = new Person[total];
+
+        for (int i = 0; i < total; i++)
+        {
+            p[i] = new Person() {
+                Name = Console.ReadLine()
+            };
+        }
+
+        for (int i = 0; i < total; i++)
+        {
+
+            Console.WriteLine(p[i].ToString()); 
+        }
+        Console.ReadKey();
+    }
+
+#region Person
+    public class Person
+    {
+        public string Name { get; set; }
+        
+        public Person (string n)
+        {
+            this.Name = n;
+        }
+
+        ~Person()
+        {
+            Name = string.Empty;
+        }
+        
+
+        public override string ToString()
+        {
+            return "Hello! My name is " + Name;
+        }
+
+    }
+#endregion
+
+
+}
---
+++ 3-Quest/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+public class StudentProfessorTest
+{
+    public static void Main(string[] args)
+    {
+        Person p1 = new Person();
+        p1.Greet();
+        
+        Student s1 = new Student();
+        s1.SetAge(15);
+        s1.Study();
+        s1.Greet();
+        s1.ShowAge();
+
+        Teacher t1 = new Teacher();
+        t1.SetAge(37);
+        t1.Greet();
+        t1.Explain();
+    }
+}
+
+#region Person
+    public class Person
+    {
+        public int age;
+        public void Greet()
+        {
+            Console.WriteLine("Hello!");
+        }
+
+        public void SetAge(int age1)
+        {
+            this.age = age1;
+        }
+    }
+#endregion
+
+#region Student
+    public class Student : Person
+    {
+        public void Study()
+        {
+            Console.WriteLine("I'm studing");
+        }
+
+        public void ShowAge()
+        {
+            Console.WriteLine("My age is: {0} years old", age);
+        }
+    }
+#endregion
+
+#region Teacher
+    public class Teacher : Person
+    {
+        public void Explain()
+        {
+            Console.WriteLine("I'm explaining");
+        }
+    }
+#endregion
