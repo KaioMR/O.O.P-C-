7e6fa931f0ac1d5c2b93975b104a5af4927d9724
Kaio Maia <98839071+KaioMR@users.noreply.github.com>
2022-02-02 12:47:24 -0300

    Add files via upload

---
+++ 1-Quest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int total = 3;
+        Person[] p = new Person[total];
+
+        for (int i = 0; i < total; i++)
+        {
+            p[i] = new Person() {
+                Name = Console.ReadLine()
+            };
+        }
+
+        for (int i = 0; i < total; i++)
+        {
+
+            Console.WriteLine(p[i].ToString()); 
+        }
+        Console.ReadKey();
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        
+        public override string ToString()
+        {
+            return "Hello! My name is " + Name;
+        }
+    }
+}
